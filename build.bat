17aa96884ee71af66f989d8bef5c73bd3e170d57
fpawel <binf@mail.ru>
2018-11-23 10:21:00 +0300

    build runbus 32 bit

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 SET dir=%HOMEDRIVE%%HOMEPATH%\.anbus
+set GOARCH=386
 buildmingw32 go build -o %dir%\anbus.exe -ldflags="-H windowsgui" github.com/fpawel/anbus/cmd
 go build -o %dir%\runbus.exe -ldflags="-H windowsgui" github.com/fpawel/anbus/cmd/run
 start %dir%
